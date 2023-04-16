323591ef511fa03bae476ebd969a885d0aa21644
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-04-16 22:58:35 +0300

    create movies-explorer app structure  template

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# movies-explorer-frontend
+# Дипломная работа
+## Level-2: Вёрстка и JSX
+В данной работе:
+
+Адрес репозитория: https://github.com/rem-ran/movies-explorer-frontend
+
---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+
+function App() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
