788936bd32f0bf967932899fdbd4a80f81b3ad1f
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-04-20 13:13:55 +0300

    make basic routing in app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,12 @@
 // import { useEffect, useState } from 'react';
-// import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 
 import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import Profile from '../Profile/Profile';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
 
 import './App.css';
 
@@ -10,7 +15,17 @@ function App() {
 
   return (
     <div className="page">
-      <Main></Main>
+      <Routes>
+        <Route path="/" element={<Main></Main>}></Route>
+        <Route path="/movies" element={<Movies></Movies>}></Route>
+        <Route
+          path="/saved-movies"
+          element={<SavedMovies></SavedMovies>}
+        ></Route>
+        <Route path="/profile" element={<Profile></Profile>}></Route>
+        <Route path="/signin" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Register></Register>}></Route>
+      </Routes>
     </div>
   );
 }
