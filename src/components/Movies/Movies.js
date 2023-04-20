16d3b09291491d480285d98ed1f27c9ea67ea79f
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-04-20 23:45:41 +0300

    download preloader

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,5 +1,8 @@
 import './Movies.css';
 
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
 const Movies = () => {
   return <div>Movies</div>;
 };
--- src/components/Movies/Preloader/Preloader.js
+++ src/components/Movies/Preloader/Preloader.js
@@ -1,7 +1,13 @@
 import './Preloader.css';
 
 const Preloader = () => {
-  return <div>Preloader</div>;
+  return (
+    <div className="preloader">
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
+    </div>
+  );
 };
 
 export default Preloader;
