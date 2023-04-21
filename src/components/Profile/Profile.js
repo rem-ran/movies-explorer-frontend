c722d1556f174feee3f43a3f96a93d1a8a2202ac
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-04-22 01:31:38 +0300

    make draft save button and validation in profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,37 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
+
+import { inputConfig } from '../../utils/constants';
+
 import './Profile.css';
 
 const Profile = () => {
-  const handleEditClick = () => {};
+  //подключаем пакет валидации форм
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: 'onBlur',
+  });
+
+  const [isEditOn, setIsEditOn] = useState(false);
+
+  const profileInputs = document.querySelectorAll('.profile__input');
+
+  const handleEditClick = (ev) => {
+    ev.preventDefault();
+    setIsEditOn(true);
+    profileInputs.forEach((input) => input.removeAttribute('disabled'));
+  };
+
+  const handleSaveClick = (ev) => {
+    ev.preventDefault();
+    setIsEditOn(false);
+    profileInputs.forEach((input) => input.setAttribute('disabled', true));
+  };
   return (
     <div>
       <Header
@@ -30,7 +58,9 @@ const Profile = () => {
           <div className="profile__form-element">
             <label className="profile__input-label">Имя</label>
             <input
+              {...register('name', inputConfig.name)}
               disabled
+              id="name"
               type="name"
               name="name"
               className="profile__input"
@@ -39,21 +69,35 @@ const Profile = () => {
           <div className="profile__form-element">
             <label className="profile__input-label">E-mail</label>
             <input
+              {...register('email', inputConfig.email)}
               disabled
+              id="email"
               type="email"
               name="email"
               className="profile__input"
             ></input>
           </div>
+          {!isEditOn ? (
+            <button onClick={handleEditClick} className="profile__edit-btn">
+              Редактировать
+            </button>
+          ) : (
+            <>
+              <span className="profile__error-txt">
+                {(errors?.name && errors.name.message) ||
+                  (errors?.email && errors.email.message)}
+              </span>
+              <button onClick={handleSaveClick} className="profile__save-btn">
+                Сохранить
+              </button>
+            </>
+          )}
         </form>
-        <div className="profile__btn-box">
-          <button onClick={handleEditClick} className="profile__edit-btn">
-            Редактировать
-          </button>
+        {!isEditOn && (
           <Link to="/signin" className="profile__logout">
             Выйти из аккаунта
           </Link>
-        </div>
+        )}
       </div>
     </div>
   );
