3890753bbc95872de1df047d470a22a4c544cc4a
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-05-01 16:50:46 +0300

    create showMore button for movies list

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,21 +1,45 @@
 // импорты
+import { useEffect, useState } from 'react';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
 // импорт стилей
 import './MoviesCardList.css';
 
-// компонет списка карточек /////////////////////////////////////
+// компонет списка карточек ////////////////////////////////////////////////
 const MoviesCardList = ({ movieCardList }) => {
+  // переменная состояния количества карточек для рендеринга
+  const [cardLimit, setCardLimit] = useState(3);
+
+  // переменная состояния рендеринга всех имеющихся карточек
+  const [allCardsRendered, setAllCardsRendered] = useState(false);
+
+  // контролируем переменную "cardLimit" для изменения переменной "allCardsRendered"
+  useEffect(() => {
+    if (cardLimit >= movieCardList.length) {
+      setAllCardsRendered(true);
+    }
+  }, [cardLimit]);
+
+  // метод обработки клика на кнопку "Ещё"
+  const handleMoreClick = () => {
+    setCardLimit(cardLimit + 3);
+  };
+  // начало JSX ////////////////////////////////////////////////////////////
   return (
     <section className="movies-list">
       <ul className="movies-list__container">
-        {movieCardList.map((movie) => (
+        {movieCardList.slice(0, cardLimit).map((movie) => (
           <MoviesCard key={movie.id} {...movie} />
         ))}
       </ul>
+      {movieCardList.length > 0 && !allCardsRendered && (
+        <button className="movies-list__more-btn" onClick={handleMoreClick}>
+          Ещё
+        </button>
+      )}
     </section>
   );
 };
 
-// экспорт //////////////////////////////////////////////////////
+// экспорт /////////////////////////////////////////////////////////////////
 export default MoviesCardList;
---
+++ test.js
@@ -0,0 +1,8 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const arrSlice = arr.slice(0, 3);
+
+console.log(arr);
+
+console.log(arrSlice);
+console.log(arr);
